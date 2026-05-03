f5a63c132df7cf9f4e2d4426092ca687a660c030
Typezera <willianbardela2131@hotmail.com>
2026-05-03 13:42:44 -0300

    documentation: Documentação dos endpoints da FriendTask

--- src/main/java/com/loginComJwt/loginJWT/controller/friendship/FriendshipController.java
+++ src/main/java/com/loginComJwt/loginJWT/controller/friendship/FriendshipController.java
@@ -2,6 +2,8 @@ package com.loginComJwt.loginJWT.controller.friendship;
 
 import com.loginComJwt.loginJWT.dto.friendship.FriendshipResponseDTO;
 import com.loginComJwt.loginJWT.service.friendship.FriendshipService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -19,32 +21,55 @@ public class FriendshipController {
         this.friendshipService = friendshipService;
     }
 
+    @Operation(
+            summary = "Envia uma solicitação de amizade para um usuário.",
+            description = "Apenas o usuário que recebeu a solicitação pode aceitar, " +
+                    "O receiver não pode enviar uma outra solicitação para o sender," +
+                    "dessa forma evitando duplicata de solicitações"
+    )
     @PreAuthorize("hasRole('USER')")
     @PostMapping("{id}")
     public ResponseEntity<String> adicionarUsuario(
+            @Parameter(description = "É necessário informar o ID do usuário que se deseja adicionar")
             @PathVariable Long id
     ){
         friendshipService.enviarSocilitacao(id);
         return ResponseEntity.ok("Solicitação enviada com sucesso.");
     }
+
+    @Operation(
+            summary = "Aceita a Solicitação",
+            description = "O receiver pode aceitar a solicitação que foi enviada," +
+                    "cada solicitação possui um ID, e ele aceita informando o ID da solicitação."
+    )
     @PreAuthorize("hasRole('USER')")
     @PatchMapping("/{id}/aceitar")
     public ResponseEntity<String> aceitarSolicitacao(
+            @Parameter(description = "ID da solicitação que deseja aceitar.")
             @PathVariable Long id
     ){
         friendshipService.aceitarSolicitacao(id);
         return ResponseEntity.ok("Solicitação aceita.");
     }
 
+    @Operation(
+            summary = "Recusa uma solicitação.",
+            description = "O receiver pode recusar uma solicitação apenas pelo ID da solicitação."
+    )
     @PreAuthorize("hasRole('USER')")
     @PatchMapping("/{id}/recusar")
     public ResponseEntity<String> recusarSolicitacao(
+            @Parameter(description = "ID da solicitação que deseja recusar")
             @PathVariable Long id
     ){
         friendshipService.recusarSolicitacao(id);
         return ResponseEntity.ok("Solicitação recusada.");
     }
 
+    @Operation(
+            summary = "Mostra todos os pedidos que o dono da conta tem",
+            description = "Exibe todas as solicitaçãoes que o dono da conta tem em pendente para aceitar/recusar"
+    )
     @PreAuthorize("hasRole('USER')")
     @GetMapping
     public ResponseEntity<List<FriendshipResponseDTO>> mostrarPedidos(){
